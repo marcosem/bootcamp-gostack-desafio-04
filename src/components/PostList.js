ed0fc1f1fdcaae8efc61fee416712f8b8d96ee0e
MARCOS EDUARDO MATHIAS <marcosem@br.ibm.com>
2019-07-03 17:42:28 -0300

    First version

---
+++ src/components/PostList.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+import PostItem from './PostItem';
+
+import Avatar1 from '../../assets/User01.jpg';
+import Avatar2 from '../../assets/User02.jpg';
+
+class PostList extends Component {
+  state = {
+    posts: [
+      {
+        id: 1,
+        author: {
+          name: 'Marcos Mathias',
+          avatar: Avatar1
+        },
+        date: '03 Jul 2019',
+        content: 'Tem alguém em casa dando comida pros cachorros?',
+        comments: [
+          {
+            id: 2,
+            author: {
+              name: 'Anezka Mathias',
+              avatar: Avatar2
+            },
+            date: '03 Jul 2019',
+            content: 'Cachorro? Que Cachorro?'
+          }
+        ]
+      }
+    ]
+
+    /*
+    posts: [
+      {
+        id: 1,
+        author: {
+          name: 'Marcos Mathias',
+          avatar: Avatar1
+        },
+        date: '03 Jul 2019',
+        content: 'Tem alguém em casa dando comida pros cachorros?',
+        comments: [
+          {
+            id: 2,
+            author: {
+              name: 'Anezka Mathias',
+              avatar: Avatar2
+            },
+            date: '03 Jul 2019',
+            content: 'Cachorro? Que Cachorro?'
+          }
+        ]
+      },
+      {
+        id: 2,
+        author: {
+          name: 'Anezka Mathias',
+          avatar: Avatar2
+        },
+        date: '04 Jul 2019',
+        content: 'Achei os cachorros!',
+        comments: [
+          {
+            id: 1,
+            author: {
+              name: 'Marcos Mathias',
+              avatar: Avatar1
+            },
+            date: '04 Jul 2019',
+            content: 'Que bom! E eles têm comida?'
+          }
+        ]
+      }
+    ]
+    */
+  };
+
+  render() {
+    const { posts } = this.state;
+
+    //console.log(posts);
+    return (
+      <div className="postlist">
+        {/*posts.map(post => (
+          <PostItem key={post.id} {...post} />
+        ))*/}
+      </div>
+    );
+  }
+}
+
+export default PostList;
